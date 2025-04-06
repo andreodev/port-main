0813a18ae1a730e80c50962f4791c05f2930f3cf
andreodev <developerandreo@gmail.com>
2025-04-06 16:47:10 -0300

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { Analytics } from "@vercel/analytics/react"
 
 // Importação do provedor de i18n
 import { I18nextProvider } from 'react-i18next';
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* Provedor de tradução i18n ao redor do App */}
     <I18nextProvider i18n={i18n}>
+      <Analytics />
       <App />
     </I18nextProvider>
   </StrictMode>,
