1df5580b560a9f639a9a29ef7c8ed6275f2b35a1
andreodev <developerandreo@gmail.com>
2025-04-22 05:57:52 -0300

    manuntenção

--- src/components/Projects/hooks/useSetupProjects.js
+++ src/components/Projects/hooks/useSetupProjects.js
@@ -1,12 +1,11 @@
 import { useTranslation } from "react-i18next";
 import one from "../../img/projetos/p1.png";
-import two from "../../img/projetos/p2.png";
 import three from "../../img/projetos/p3.png";
 import four from "../../img/projetos/p4.png";
 import five from "../../img/projetos/pj5.png";
 import six from "../../img/projetos/pj6.png"
 
-const images = [one, two, three, four, five, six];
+const images = [one,  three, four, five, six];
 
 export function useSetupProjects() {
   const { t } = useTranslation(); // ✅ Hook dentro da função
