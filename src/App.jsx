50a3d9943efb6e3764793a699d1b0f499ceaa847
andreodev <developerandreo@gmail.com>
2025-07-02 21:06:12 -0300

    new background

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { ThemeProvider } from "./context/ThemeProvider";
 import Route from "./rotas/routes";
 import './context/style.css'
 import { Analytics } from "@vercel/analytics/react"
+import ParticlesBackground from "./components/BgParticles/index.jsx";
 
 export default function App() {
   return (
     <ThemeProvider>
+      <ParticlesBackground />
       <Analytics />
       <Route />
     </ThemeProvider>
---
+++ src/components/BgParticles/index.jsx
@@ -0,0 +1,48 @@
+import { useCallback } from 'react';
+import Particles from 'react-tsparticles';
+import { loadSlim } from 'tsparticles-slim'; // Alternativa mais leve que loadFull
+
+export default function ParticlesBackground() {
+  const particlesInit = useCallback(async (engine) => {
+    console.log('Engine:', engine); // Para verificação
+    await loadSlim(engine); // Usando loadSlim em vez de loadFull
+  }, []);
+
+  return (
+    <div style={{ position: 'relative' }}>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        options={{
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: { enable: true, mode: "push" },
+              onHover: { enable: true, mode: "repulse" }
+            }
+          },
+          particles: {
+            color: { value: "#ffffff" },
+            links: { 
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1
+            },
+            move: {
+              enable: true,
+              speed: 2,
+              direction: "none"
+            },
+            number: { density: { enable: true }, value: 80 },
+            opacity: { value: 0.5 },
+            shape: { type: "circle" },
+            size: { value: { min: 1, max: 3 } }
+          }
+        }}
+      />
+      
+    </div>
+  );
+}
